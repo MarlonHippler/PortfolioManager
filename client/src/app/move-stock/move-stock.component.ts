609d747f8d56206c2491aed4a3427a5402320697
Marc Wortmann <marc.wortmann@nordakademie.de>
2022-02-06 17:45:09 +0100

    move stock maske

--- client/src/app/move-stock/move-stock.component.ts
+++ client/src/app/move-stock/move-stock.component.ts
@@ -1,10 +1,55 @@
-import {Component, OnInit, Output, EventEmitter} from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ActivatedRoute, Router} from '@angular/router';
+import {AlertService} from '../services/alert.service';
+import {AuthenticationService} from '../services/authentication.service';
 
 @Component({
-    selector: 'app-move-stock-dialog',
+    selector: 'move-stock',
     templateUrl: './move-stock.component.html',
     styleUrls: ['./move-stock.component.css']
 })
-export class MoveStockComponent {
+export class MoveStockComponent implements OnInit {
+
+    moveStockForm: FormGroup;
+    loading = false;
+    submitted = false;
+    returnUrl: string;
+
+    constructor(private formBuilder: FormBuilder,
+                private route: ActivatedRoute,
+                private router: Router,
+                private authenticationService: AuthenticationService,
+                private alertService: AlertService) {
+
+    }
+
+    ngOnInit() {
+        this.moveStockForm = this.formBuilder.group({
+            name: ['', Validators.required],
+            quantity: ['', Validators.required],
+            wkn: ['', Validators.required],
+
+
+        });
+        // get return url from route parameters or default to '/'
+        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+    }
+    // convenience getter for easy access to form fields
+    get f() { return this.moveStockForm.controls; }
+
+    onSubmit() {
+
+        this.submitted = true;
+
+
+        // stop here if form is invalid
+        if (this.moveStockForm.invalid) {
+            return;
+        }
+
+
+    }
 
 }
