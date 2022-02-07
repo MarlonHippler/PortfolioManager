1629aa9db122417c51a6952bf8095d8a030c2c57
Marc Wortmann <marc.wortmann@nordakademie.de>
2022-02-07 20:22:33 +0100

    updates

---
+++ src/main/java/com/apple/PortfolioManager/controller/PortfolioListController.java
@@ -0,0 +1,24 @@
+package com.apple.PortfolioManager.controller;
+
+import com.apple.PortfolioManager.repo.PortfolioRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+
+    @Controller
+    public class PortfolioListController {
+
+        @Autowired
+        private PortfolioRepo eRepo;
+
+        @GetMapping({"/list"})
+        public ModelAndView getAllPortfolios() {
+            ModelAndView mav = new ModelAndView("list-portfolios");
+            mav.addObject("portfolios", eRepo.findAll());
+            return mav;
+        }
+    }
+}
